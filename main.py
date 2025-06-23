ae1560808d6c6ad3a4e66e6c6b6b87dc22e7b512
ghost-mann <iamaob8@protonmail.com>
2025-06-23 13:20:42 +0300

    feat: added parameters to get_latest_prices function in main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import time
 
 BASE_URL = "https://api.binance.com"
 
-def get_latest_prices():
+def get_latest_prices(symbol='BTCUSDT'):
     url = f'{BASE_URL}/api/v3/ticker/price'
-    response = requests.get(url)
+    params = {
+        'symbol':symbol.upper()
+    }
+    response = requests.get(url,params=params)
     data = response.json()
     df_prices = pd.DataFrame(data)
     print(df_prices)
@@ -19,7 +22,7 @@ def order_book(symbol='BTCUSDT',limit=5):
     }
     response = requests.get(url, params=params)
     order_data = response.json()
-    order_df = pd.DataFrame([order_data])
+    order_df = pd.DataFrame(order_data)
     print(order_df)
     return order_df
 
