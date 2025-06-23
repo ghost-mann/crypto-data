68308ea83dc82a9982fcc7a715e1b17d5aaff233
ghost-mann <iamaob8@protonmail.com>
2025-06-23 13:14:16 +0300

    feat: added order book function in main.py

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ def get_latest_prices():
     url = f'{BASE_URL}/api/v3/ticker/price'
     response = requests.get(url)
     data = response.json()
-    df = pd.DataFrame(data)
-    print(df)
+    df_prices = pd.DataFrame(data)
+    print(df_prices)
+    return df_prices
     
-def order_book():
-    
+def order_book(symbol='BTCUSDT',limit=5):
+    url = f'{BASE_URL}/api/v3/depth'
+    params = {
+        'symbol':symbol.upper(),
+    }
+    response = requests.get(url, params=params)
+    order_data = response.json()
+    order_df = pd.DataFrame([order_data])
+    print(order_df)
+    return order_df
+
+get_latest_prices()
+order_book()
+
